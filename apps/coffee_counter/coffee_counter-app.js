7cd0341075664e7da5ea657c4be1ef4744641df0
Niklas Zeidler <github@niklas.firlefanz.com>
2022-10-19 10:44:46 +0200

    fixing icons pt II

--- apps/coffee_counter/coffee_counter-app.js
+++ apps/coffee_counter/coffee_counter-app.js
@@ -2,6 +2,7 @@ var counter = 30;
 var counterInterval;
 var file = require("Storage").open("coffee_log.csv","a");
 var finish_message = "Coffee done";
+
 function outOfTime() {
   if (counterInterval) return;
   E.showMessage(finish_message, "My coffee");
@@ -71,5 +72,35 @@ function showMainMenu() {
   };
   E.showMenu(menu);
 }
+// clear the screen
+g.clear();
 
+var n = 0;
+
+// redraw the screen
+function draw() {
+  g.reset().clearRect(Bangle.appRect);
+  g.setFont("6x8").setFontAlign(0,0).drawString("Up / Down",g.getWidth()/2,g.getHeight()/2 - 20);
+  g.setFont("Vector",60).setFontAlign(0,0).drawString(n,g.getWidth()/2,g.getHeight()/2 + 30);
+}
+
+// Respond to user input
+Bangle.setUI({mode: "updown"}, function(dir) {
+  if (dir<0) {
+    n--;
+    draw();
+  } else if (dir>0) {
+    n++;
+    draw();
+  } else {
+    n = 0;
+    draw();
+  }
+});
+// Load widgets
+Bangle.loadWidgets();
+Bangle.drawWidgets();
+
+// First draw...
+draw();
 showMainMenu();
